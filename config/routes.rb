35c98cebded86b526e402242bd112a820d3b48ca
khalilhamdii <khaalil.hamdi@gmail.com>
2020-10-26 14:31:34 +0100

    Add posts/new view file + create post  migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
-  resources :users, only: %i[new create index]
+  
+  resources :posts, only: %i[new create index]
+  root to: "posts#index"
 end
--- db/migrate/20201027122857_create_posts.rb
+++ db/migrate/20201027122857_create_posts.rb
@@ -1,9 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.text :author
-      t.text :title
-      t.text :body  
+      t.text :post
       t.timestamps
     end
   end

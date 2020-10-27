1cd82c62668773770a9dea11c801384eb630bb38
Daniel Ronan <danielconnorronan@gmail.com>
2020-10-27 13:57:03 +0000

    created working post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,29 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :create]
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: %i[new create index]
 
   def index
-    @posts = Post.all.order("created_at DESC")
-    @post = Post.new
+    @posts = Post.all
   end
+
+  def show; end
+
+  def edit; end
+  
+  def destroy; end
   
   def create
-    
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to new_post_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def new
@@ -18,15 +33,11 @@ class PostsController < ApplicationController
 
 private
 
-def set_post
-  @post = Post.find(params[:id])
-end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-
-
-def post_params
-  params.require(:post).permit(:post)
-end
-
-
+  def post_params
+    params.require(:post).permit(:post, :user_id)
+  end
 end
